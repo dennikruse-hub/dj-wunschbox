8f3a976035541f76db84e42f064e4df3212be704
dennikruse-hub <denni.kruse@gmail.com>
2026-07-06 22:31:06 +0200

    Update login.js

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,9 +1,17 @@
-import { getLoginUrl } from '../../lib/spotify';
-
 export default function handler(req, res) {
-  try {
-    res.redirect(getLoginUrl());
-  } catch (err) {
-    res.status(500).send(err.message);
-  }
+  const scopes = [
+    'playlist-read-private',
+    'playlist-read-collaborative',
+    'playlist-modify-private',
+    'playlist-modify-public'
+  ].join(' ');
+
+  const params = new URLSearchParams({
+    response_type: 'code',
+    client_id: process.env.SPOTIFY_CLIENT_ID,
+    scope: scopes,
+    redirect_uri: process.env.SPOTIFY_REDIRECT_URI
+  });
+
+  res.redirect('https://accounts.spotify.com/authorize?' + params.toString());
 }
